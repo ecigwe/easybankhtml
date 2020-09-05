2cdabd66dcb6404c6bd7ea7bdfef6a662ef54616
emmanuel <emmanuel.igwe38@gmail.com>
2020-09-05 02:54:16 +0100

    article

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("hello world emmanuel");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
+btnHamburger.addEventListener("click", function () {
+  console.log("btn hamburger");
+
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+  } else {
+    //open hanburger
+    body.classList.add("nosroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
+});
